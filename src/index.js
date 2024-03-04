47ab2809b50f534f3fb03e20ef0bad95b380dc26
Raul Onitza-Klugman <raulonita@gmail.com>
2024-03-04 18:28:49 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+
+import * as github from '@actions/github';
+
+async function run() {
+    const context = github.context;
+
+    // const runCmd: string = core.getInput('run-cmd');
+    // const prMessage: string = core.getInput('pr-message');
+    if (!issueMessage && !prMessage) {
+      throw new Error(
+        'Action must have at least one of issue-message or pr-message set'
+      );
+    }
+
+    const payload = context.payload.pull_request;
+
+    exec.exec(`node ${payload}`);
+}
+
+run();
